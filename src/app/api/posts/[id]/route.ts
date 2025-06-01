9028b0c61a930e064f4005ad315948e9537faec6
Anci4nt <neeru.alokik@gmail.com>
2025-06-01 13:01:15 +0530

    Update route.ts

--- src/app/api/posts/[id]/route.ts
+++ src/app/api/posts/[id]/route.ts
@@ -1,11 +1,11 @@
-import { PrismaClient } from '@prisma/client'
 import { NextRequest, NextResponse } from 'next/server'
+import { prisma } from '@/lib/prisma'
 
-const prisma = new PrismaClient()
-
-export async function DELETE(req: NextRequest, context: { params: { id: string } }) {
-  const idParam = context.params.id
-  const postId = parseInt(idParam)
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const postId = parseInt(params.id)
 
   if (isNaN(postId)) {
     return NextResponse.json({ error: 'Invalid ID' }, { status: 400 })
@@ -14,7 +14,7 @@ export async function DELETE(req: NextRequest, context: { params: { id: string }
   try {
     await prisma.post.delete({ where: { id: postId } })
     return NextResponse.json({ message: 'Post deleted' }, { status: 200 })
-  } catch (error) {
+  } catch {
     return NextResponse.json({ error: 'Post not found' }, { status: 404 })
   }
 }
